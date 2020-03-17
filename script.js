1629ea631c24de06452903fe09d1399c4bf36dc0
ibra nurlette <ibraputra843@gmail.com>
2020-03-17 12:49:03 +0700

    search film with public Api

---
+++ script.js
@@ -0,0 +1,87 @@
+// function untuk cari film
+function search_movie(){
+    $('#movie-list').html('');
+    // gunakan ajax u/ amabil api
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': 'e352744d',
+            's': $('#search-input').val()
+        },
+        success: function(result){
+            if(result.Response == 'True'){
+                let movies = result.Search;
+                $.each(movies, function(i, data){
+                    $('#movie-list').append(`
+                        <div class="col-md-4">
+                        <div class="card mb-3" >
+                          <img src="`+data.Poster+`" class="card-img-top">
+                          <div class="card-body">
+                            <h5 class="card-title">`+data.Title+`</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">`+data.Year+`</h6>
+                            <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="`+data.imdbID+`">see detail</a>
+                          </div>
+                        </div>
+                        </div>
+                        `);
+                })
+                $('#search-input').val('')
+                // tampilkan datanya kalau gk ada
+            }else{
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">` + result.Error + `</h1>
+                    </div>
+                    `)
+            }
+        }
+    });
+}
+// saar tombol searcnya di klik
+$('#search-button').on('click', function(){
+    search_movie();
+});
+// saaat tombol enteer di tekan
+$('#search-input').on('keyup', function(e){
+    if(e.keyCode === 13){
+        search_movie();
+    }
+});
+// even binding
+$('#movie-list').on('click','.see-detail', function(){
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType : 'json',
+        data: {
+            'apikey':'e352744d',
+            'i': $(this).data('id')
+        },
+        success: function(movie){
+            if(movie.Response === 'True'){
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="`+movie.Poster+`" class="img-fluid">
+                            </div>
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                  <li class="list-group-item">`+movie.Title+`</li>
+                                  <li class="list-group-item">Released :`+movie.Released+`</li>
+                                  <li class="list-group-item">Genre :`+movie.Genre+`</li>
+                                  <li class="list-group-item">Director :`+movie.Director+`</li>
+                                  <li class="list-group-item">Actors :`+movie.Actors+`</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+            }
+        }
+    })
+})
+
+
